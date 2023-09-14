498e7bd3e9ead2b2de719a56ef0b4818fa06a825
justinb91011 <123137849+justinb91011@users.noreply.github.com>
2023-09-14 12:50:24 -0400

    Update Todo.js

--- src/Todo.js
+++ src/Todo.js
@@ -1,17 +1,15 @@
-class Todo{
-    // id
-    // text 
-    // completed
-    static nextId = 1;
+class Todo {
+  static nextId = 1;
 
-    constructor(text){
-        this.id = Todo.nextId++;
-        this.text = text;
-        this.completed = false;
-    }
+  constructor(text) {
+    this.id = Todo.nextId++;
+    this.text = text;
+    this.completed = false;
+  }
 
-
-    toggle(){
-        this.completed = !this.completed;
-    }
+  toggle() {
+    this.completed = !this.completed;
+  }
 }
+
+export default Todo;
